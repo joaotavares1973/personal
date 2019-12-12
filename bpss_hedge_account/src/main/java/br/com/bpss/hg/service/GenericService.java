0733887912fe1224cf8a522c4c40b689d69769b6
jpbt@uol.com.br <jpbt@uol.com.br>
2019-12-12 13:20:10 -0300

    out

--- bpss_hedge_account/src/main/java/br/com/bpss/hg/service/GenericService.java
+++
@@ -1,7 +0,0 @@
-package br.com.bpss.hg.service;
-
-public interface GenericService {
-	
-	public Object getParametrosView();
-	
-}
--- bpss_hedge_account/src/main/java/br/com/bpss/hg/service/PermissoesService.java
+++
@@ -1,7 +0,0 @@
-package br.com.bpss.hg.service;
-
-public interface PermissoesService {
-	
-	public boolean permicaoAutorizada(String usuario, String idPermissao);
-	
-}
